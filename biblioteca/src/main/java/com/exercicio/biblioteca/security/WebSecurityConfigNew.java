d211a5787c5c9e02b23bfc942bef594e949976b6
AthosDeveloper <athos.silva@zup.com.br>
2023-09-12 15:04:46 -0300

    alterando a aplicação

--- biblioteca/src/main/java/com/exercicio/biblioteca/security/WebSecurityConfigNew.java
+++ biblioteca/src/main/java/com/exercicio/biblioteca/security/WebSecurityConfigNew.java
@@ -2,8 +2,6 @@ package com.exercicio.biblioteca.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,11 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfigNew {
     @Bean
 public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-http.httpBasic().and().authorizeHttpRequests() .antMatchers(HttpMethod.POST,  "/api/livros").hasRole("BIBLIOTECARIO")
-        .antMatchers(HttpMethod.GET, "/api/livros").hasAnyRole("USER", "BIBLIOTECARIO")
-        .antMatchers(HttpMethod.PUT, "/api/livros").hasRole("BIBLIOTECARIO")
-        .antMatchers(HttpMethod.DELETE, "/api/livros").hasRole("BIBLIOTECARIO")
-        .anyRequest().authenticated().and().csrf().disable();
+http.httpBasic().and().authorizeHttpRequests().anyRequest().authenticated().and().csrf().disable();
 return  http.build();
     }
     @Bean
